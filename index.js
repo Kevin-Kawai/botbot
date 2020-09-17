62680c87a7f76db32f957f6ae6b339641d005b00
Kevin-Kawai <kevin.fredricks@gmail.com>
2020-09-17 15:52:25 +0900

    add express

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000
+
+app.use('/', (req, res) => {
+  res.sendStatus(200)
+})
 
 client.on('ready', () => {
   console.log('I am rdy')
