802d7fab829342b7f3ad9d2256ce264cf22f9a4d
KevinKawai <kevin.fredricks@gmail.com>
2020-10-24 14:39:19 +0900

    fix data response

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ client.on('message', message => {
     init()
       .then((data) => {
         console.log(data)
-        client.channels.cache.find((channel) => { return channel.name === 'general'}).send(res.dat.joke)
+        client.channels.cache.find((channel) => { return channel.name === 'general'}).send(data.joke)
       })
   }
 })
